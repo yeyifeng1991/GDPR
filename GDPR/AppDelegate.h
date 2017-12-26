15912829d73ac689414008509334ae6c2c2fff34
zxl <zxl@ttjj-ios.local>
2017-12-26 11:04:24 +0800

    Initial Commit

---
+++ GDPR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GDPR
+//
+//  Created by zxl on 2017/12/26.
+//  Copyright © 2017年 EM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GDPR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GDPR
+//
+//  Created by zxl on 2017/12/26.
+//  Copyright © 2017年 EM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
